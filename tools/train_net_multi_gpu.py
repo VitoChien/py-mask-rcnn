d2948fc850a98a18a49e211d5966f2adf4f0fa7f
VitoChien <406618818@qq.com>
2018-10-13 16:41:06 +0800

    fix bug

--- tools/train_net_multi_gpu.py
+++ tools/train_net_multi_gpu.py
@@ -33,7 +33,7 @@ def parse_args():
                         help='number of iterations to train',
                         default=40000, type=int)
     parser.add_argument('--weights', dest='pretrained_model',
-                        help='initialize with pretrained model weights'
+                        help='initialize with pretrained model weights',
                         default=None, type=str)
     parser.add_argument('--cfg', dest='cfg_file',
                         help='optional config file',
